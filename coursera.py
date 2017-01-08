979dc4107c880327da717fba2275f0677a3901a4
SkayLarsen <SkayLarsen@gmail.com>
2017-01-08 20:36:59 +0300

    задача #10

--- coursera.py
+++ coursera.py
@@ -1,16 +1,68 @@
+import argparse
+import json
+import requests
+import random
+from bs4 import BeautifulSoup
+from lxml import etree
+from openpyxl import Workbook
 
 
-def get_courses_list():
-    pass
+def get_course_title(soup):
+    return soup.find("div", "course-title").text
 
 
-def get_course_info(course_slug):
-    pass
+def get_course_lang(soup):
+    return soup.find("div", "language-info").text.split(',')[0]
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def get_course_start(soup):
+    try:
+        json_data = soup.select('script[type="application/ld+json"]')[0].text
+        return json.loads(json_data)['hasCourseInstance'][0]['startDate']
+    except (KeyError, IndexError):
+        return "Нет данных"
+
+
+def get_course_duration(soup):
+    duration = len(soup.find_all("div", "week"))
+    return duration if duration > 0 else "Нет данных"
+
+
+def get_course_rate(soup):
+    try:
+        return soup.find('div', "ratings-text").text
+    except AttributeError:
+        return "Нет данных"
+
+def get_course_info(course_url):
+    course_page = requests.get(course_url).content.decode("utf-8", "ignore")
+    soup = BeautifulSoup(course_page, "html.parser")
+    return [get_course_title(soup), get_course_lang(soup), get_course_start(soup),
+            get_course_duration(soup), get_course_rate(soup)]
+
+
+def get_courses_list(number_of_courses=20):
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    courses_xml = requests.get(url).content
+    tree = etree.fromstring(courses_xml)
+    courses_total = len(tree)
+    course_numbers = random.sample(range(courses_total), number_of_courses)
+    return [tree[number][0].text for number in course_numbers]
+
+
+def output_courses_info_to_xlsx(filepath, courses_info):
+    book = Workbook()
+    sheet = book.active
+    sheet.title = "Курсы"
+    sheet.append(['Название', 'Язык', 'Дата начала', 'Количество недель', 'Средняя оценка'])
+    for course in courses_info:
+        sheet.append(course)
+    book.save(filepath)
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description="Сохранение информации о курсах с Coursera в xlsx-файл")
+    parser.add_argument('filepath', help='путь к xlsx-файлу')
+    args = parser.parse_args()
+    courses = [get_course_info(url) for url in get_courses_list()]
+    output_courses_info_to_xlsx(args.filepath, courses)
